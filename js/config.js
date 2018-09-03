4a53bbcc2760b6cff7f512761590c08dcc368cff
Dharmik Shah <dharm1k987@gmail.com>
2018-09-03 17:52:39 -0400

    add c

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var config_obj = {
+    github_token : "52589fc25b9af63e31c6128f0f48315038f78550", 
+    ta_uri : "/repos/dharm1k987/TA_FILES/contents",
+    commit_name : "Dharmik Shah",
+    commit_email : "dharm1k987@gmail.com",
+    upload_pwd: "demouser"
+}
